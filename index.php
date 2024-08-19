59f7c5309ab681d75bc5a2be242048325713539b
Jerry Nwobodo <163215884+olivestackscode@users.noreply.github.com>
2024-08-19 05:02:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,1 @@
-<html>
-  <head>
-    
-<title>
-Welcome to Test
-</title>
-  <style>
-    body{
-      background-color: blue;
-    }
-  </style>
-  </head>
-    <body>
- <h1>
-Welcome to Test
-</h1>   
-  </body>
 
-</html>
